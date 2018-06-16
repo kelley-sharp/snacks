d8ef6064346045155ea3ea669c1a6c74a6c624ac
kelley <kelleylsharp@gmail.com>
2018-06-16 13:39:37 -0700

    add new snack is implemented

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, url_for, render_template
+from flask import Flask, request, url_for, render_template, redirect
 from flask_modus import Modus
 
 app = Flask(__name__)
@@ -17,6 +17,7 @@ popcorn = Snack("2", "Popcorn", "Salty")
 soda = Snack("3", "Soda", "Sweet")
 
 snacks = [chocolate, popcorn, soda]
+snack_id = 1
 
 
 @app.route("/snacks", methods=["GET"])
@@ -29,7 +30,16 @@ def show_form():
     return render_template("form.html")
 
 
-@app.route("/snacks", methods=["PUT"])
+@app.route("/snack", methods=["GET"])
+def show_snack():
+    return render_template("snack.html")
+
+
+@app.route("/form", methods=["PUT"])
 def add_new_snack():
-    snack_name = request.values.get("name")
-    snack_kind = request.values.get("kind")
+    global snack_id
+    new_snack = Snack(
+        id=snack_id, name=request.form["name"], kind=request.form["kind"])
+    snacks.append(new_snack)
+    snack_id += 1
+    return redirect(url_for("display_snacks"))
