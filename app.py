6f30bfd8c6320407eae5b9d1888daa3eab6fb747
kelley <kelleylsharp@gmail.com>
2018-06-17 22:06:38 -0700

    CRUD complete

--- app.py
+++ app.py
@@ -6,18 +6,18 @@ modus = Modus(app)
 
 
 class Snack:
-    def __init__(self, id, name, kind):
-        self.id = id
+    def __init__(self, snack_id, name, kind):
+        self.id = snack_id
         self.name = name
         self.kind = kind
 
 
-chocolate = Snack("1", "Chocolate", "Sweet")
-popcorn = Snack("2", "Popcorn", "Salty")
-soda = Snack("3", "Soda", "Sweet")
+chocolate = Snack(1, "Chocolate", "Sweet")
+popcorn = Snack(2, "Popcorn", "Salty")
+soda = Snack(3, "Soda", "Sweet")
 
 snacks = [chocolate, popcorn, soda]
-snack_id = 1
+snack_id = 4
 
 
 @app.route("/snacks", methods=["GET"])
@@ -30,16 +30,43 @@ def show_form():
     return render_template("form.html")
 
 
-@app.route("/snack", methods=["GET"])
-def show_snack():
-    return render_template("snack.html")
+@app.route("/snacks/<int:id>", methods=["GET"])
+def show_snack(id):
+
+    found_snack = [snack for snack in snacks if snack.id == id][0]
+    return render_template("snack.html", snack=found_snack)
 
 
 @app.route("/form", methods=["PUT"])
 def add_new_snack():
     global snack_id
     new_snack = Snack(
-        id=snack_id, name=request.form["name"], kind=request.form["kind"])
+        snack_id=snack_id,
+        name=request.form["name"],
+        kind=request.form["kind"])
     snacks.append(new_snack)
     snack_id += 1
     return redirect(url_for("display_snacks"))
+
+
+@app.route("/snacks/<int:id>", methods=["DELETE"])
+def remove_snack(id):
+    global snack_id
+    found_snack = [snack for snack in snacks if snack.id == id][0]
+    snacks.remove(found_snack)
+    return redirect(url_for("display_snacks"))
+
+
+@app.route("/snacks/<int:id>/edit", methods=["GET"])
+def edit_snack(id):
+    global snack_id
+    found_snack = [snack for snack in snacks if snack.id == id][0]
+    return render_template("edit.html", snack=found_snack)
+
+
+@app.route("/snacks/<int:id>", methods=["PATCH"])
+def update_snack(id):
+    found_snack = [snack for snack in snacks if snack.id == id][0]
+    found_snack.name = request.form['name']
+    found_snack.kind = request.form['kind']
+    return redirect(url_for("show_snack", id=found_snack.id))
